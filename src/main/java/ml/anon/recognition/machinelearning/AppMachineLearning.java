568c96f44a0d473501eff2178f6176a84d36f51f
Mirco <ablx@posteo.de>
2017-05-13 17:50:51 +0200

    Spring Boot Boilerplate

---
+++ src/main/java/ml/anon/recognition/machinelearning/AppMachineLearning.java
@@ -0,0 +1,13 @@
+package ml.anon.recognition.machinelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMachineLearning {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMachineLearning.class, args);
+
+    }
+}
