0113698d6730cd0f0948bfec25d3a1d7032927df
mschild <matthiass2006@web.de>
2017-08-27 15:17:46 +0200

    anonPlusTokens added

---
+++ api/src/main/java/ml/anon/recognition/machinelearning/model/AnonPlusTokens.java
@@ -0,0 +1,17 @@
+package ml.anon.recognition.machinelearning.model;
+
+import lombok.Builder;
+import lombok.Data;
+import ml.anon.anonymization.model.Anonymization;
+
+import java.util.List;
+
+@Data
+@Builder
+public class AnonPlusTokens {
+
+
+    private Anonymization anonymization;
+    private List<String> tokens;
+
+}
